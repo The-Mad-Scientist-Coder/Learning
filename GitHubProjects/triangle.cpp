a85da0706fd2528b6765f9c107130c6c2b0a9833
The-Mad-Scientist-Coder <viktoriaslep0@gmail.com>
2026-04-01 14:55:13 +0200

    Класс треугольник
    Первичный вариант работы классов с созданием треугольника и узнавания его периметра и плошади.

---
+++ GitHubProjects/triangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int n;
+cin >> n;
+
+for (int i=1; i<=n; i++){
+
+    for (int a=1; a<=i; a++){
+
+        cout << i << " ";
+
+    }
+
+    cout << "" << endl;
+
+}
+
+
+
+
+
+
+    return 0;
+}
